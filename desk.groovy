dd088e41409201fad45b2fe1388bb3a730e51ff0
lizeyang <610556220@qq.com>
2020-08-27 08:36:06 +0800

    Add files via upload

---
+++ desk.groovy
@@ -0,0 +1,103 @@
+#!groovy
+
+@Library('jenkinslib') _
+
+def tools = new org.devops.tools()
+
+
+
+String workspace = "/opt/jenkins/workspace"
+
+//Pipeline
+pipeline {
+    agent { node {  label "master"   //指定运行节点的标签或者名称
+                    customWorkspace "${workspace}"   //指定运行工作目录（可选）
+            }
+    }
+
+    options {
+        timestamps()  //日志会有时间
+        skipDefaultCheckout()  //删除隐式checkout scm语句
+        disableConcurrentBuilds() //禁止并行
+        timeout(time: 1, unit: 'HOURS')  //流水线超时设置1h
+    }
+
+    stages {
+        //下载代码
+        stage("GetCode"){ //阶段名称
+            when { environment name: 'test', value: 'abcd' }
+            steps{  //步骤
+                timeout(time:5, unit:"MINUTES"){   //步骤超时时间
+                    script{ //填写运行代码
+                        println('获取代码')
+                        println("${test}")
+                        
+                        input id: 'Test', message: '我们是否要继续？', ok: '是，继续吧！', parameters: [choice(choices: ['a', 'b'], description: '', name: 'test1')], submitter: 'lizeyang,admin'
+                    }
+                }
+            }
+        }
+
+        stage("01"){
+            failFast true
+            parallel {
+        
+                //构建
+                stage("Build"){
+                    steps{
+                        timeout(time:20, unit:"MINUTES"){
+                            script{
+                                println('应用打包')
+                                
+                                mvnHome = tool "m2"
+                                println(mvnHome)
+                                
+                                sh "${mvnHome}/bin/mvn --version"
+                            }
+                        }
+                    }
+                }
+        
+                //代码扫描
+                stage("CodeScan"){
+                    steps{
+                        timeout(time:30, unit:"MINUTES"){
+                            script{
+                                print("代码扫描")
+
+                                tools.PrintMes("this is my lib!")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    //构建后操作
+    post {
+        always {
+            script{
+                println("always")
+            }
+        }
+
+        success {
+            script{
+                currentBuild.description = "\n 构建成功!" 
+            }
+        }
+
+        failure {
+            script{
+                currentBuild.description = "\n 构建失败!" 
+            }
+        }
+
+        aborted {
+            script{
+                currentBuild.description = "\n 构建取消!" 
+            }
+        }
+    }
+}
